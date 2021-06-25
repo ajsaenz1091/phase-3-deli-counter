9854bab3b03cc24324b561fa5dec9227a5af0bbb
Alberto <ajsaenz1091@gmail.com>
2021-06-25 17:20:56 -0400

    Completed assignment

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+def line(customers)
+    if customers.length > 0
+        puts "The line is currently: #{customers.map.with_index(1){|customer, idx| "#{idx}. #{customer}"}.join(' ')}"
+    else 
+        puts "The line is currently empty."
+    end
+end
+
+def take_a_number(line, name)
+    line << name
+    puts "Welcome, #{name}. You are number #{line.length} in line."
+end
+
+def now_serving(line)
+    if line.empty?
+        puts "There is nobody waiting to be served!"
+    else 
+        puts "Currently serving #{line.shift()}."
+    end
+end
